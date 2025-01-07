6d67b3b8a58b48bace7ee95d3c2579076bb1fce2
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-06 22:05:07 -0300

    logica 3

--- Lógica 2/main.c
+++ Lógica 2/main.c
@@ -3,6 +3,11 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int ano_nascimento = 1998;
+    int ano_atual = 2025;
+    int resultado = (ano_atual - ano_nascimento);
+
+    printf("O DANIEL tem %d anos", resultado);
+
     return 0;
 }
