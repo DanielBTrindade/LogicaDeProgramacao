996e2a9159125c553651de5d4c9bcbf0c130b2a9
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-07 22:09:53 -0300

    aprendendo logica

--- Lógica 5/main.c
+++ Lógica 5/main.c
@@ -8,6 +8,7 @@ int main()
 
     char nome[256];
     char sobrenome[256];
+    char cidade[256];
     int ano_nascimento;
     int idade;
     int ano_atual;
@@ -17,17 +18,22 @@ int main()
     printf("\nE seu sobrenome?\n");
     scanf("%s", sobrenome);
 
-    printf("\nShow de bola %s %s. Qual a sua idade?\n", nome, sobrenome);
+    printf("\nShow de bola, %s %s. Quantos anos você faz essa ano?\n", nome, sobrenome);
     scanf("%d",&idade);
-    printf("\nLegal! %s %s tem %d anos\n", nome, sobrenome, idade);
+    printf("\nLegal! %s %s você tem %d anos.\n", nome, sobrenome, idade);
 
     printf("\nEm que ano nos estamos?\n");
-    scanf("%d", ano_atual);
+    scanf("%d", &ano_atual);
 
-    ano_nascimento = ano_atual-idade;
 
-    printf("\nLegal! então você nasceu em %d \n", ano_nascimento);
+    printf("\nLegal! então você nasceu em %d.\n", ano_nascimento);
 
+    printf("\nOnde você mora?\n");
+    scanf("%s", cidade);
+
+    printf("\nEu tambem, %s é uma otima cidade.\n", cidade);
+
+    printf("\nBom te conhecer %s %s, de %s que nasceu em %d e tem %d anos\n", nome, sobrenome, cidade, ano_nascimento, idade);
 
     return 0;
 }
