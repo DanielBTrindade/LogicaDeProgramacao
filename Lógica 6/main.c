3b053e412846c1188c1ab8970098ca501087d890
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-08 18:36:38 -0300

    treinando logica

--- Lógica 6/main.c
+++ Lógica 6/main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL, "portuguese");
+
     char nome[256];
     int idade;
 
-    printf("Qual seu nome?\n");
+    printf("Olá! Qual seu nome?\n");
     scanf("%s", &nome);
 
     printf("\nQual sua idade?\n");
     scanf("%d", &idade);
 
-    printf("Nome: %s \n Idade: %d\n", nome, idade);
+    printf("\nNome: %s \nIdade: %d\n", nome, idade);
+    printf("\nAguarde...\n");
+    printf("\nA primeira letra do seu nome é: %c\n", nome[0]);
 
+    if(idade >= 65){
+        printf("Você é idoso!");
+    }else if(idade >= 18){
+        printf("\nVocê é adulto!\n");
+    }else if(idade >= 12){
+        printf("\nVocê é adolecente!\n");
+    }else if(idade > 3 ){
+        printf("\nVocê é criança!\n");
+    }else{
+        printf("Você é um bebê");
+    }
 
     return 0;
 }
